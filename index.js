b2b1ae293a5a65474f24c19d0214de0fddff74e5
yeex <mozayntwali@gmail.com>
2018-11-20 00:13:58 +0200

    Develop

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var app = express();
 app.use(express.json());
 const parcels = [
 	{
-		id: "1",
+		id: 1,
 		userName: "moise",
 		parcels: [
 		  {
-			parcelId: '11',
+			parcelId: 11,
 			parcelName: 'phones',
 			status: 'delivered',
 			location: 'rwanda',
@@ -16,7 +16,7 @@ const parcels = [
 			price: 200
 		  },
 		  {
-			parcelId: '13',
+			parcelId: 13,
 			parcelName: 'phones',
 			status: 'cancelled',
 			location: 'rwanda',
@@ -25,7 +25,7 @@ const parcels = [
 			price: 200
 		  },
 		  {
-			parcelId: '12',
+			parcelId: 12,
 			parcelName: 'phones',
 			status: 'in transit',
 			location: 'rwanda',
@@ -36,11 +36,11 @@ const parcels = [
 		]
 	},
 	{
-		id: "2",
+		id: 2,
 		userName: "moise",
 		parcels: [
 		  {
-			parcelId: '21',
+			parcelId: 21,
 			parcelName: 'phones',
 			status: 'in transit',
 			location: 'rwanda',
@@ -49,7 +49,7 @@ const parcels = [
 			price: 200
 		  },
 		  {
-			parcelId: '25',
+			parcelId: 25,
 			parcelName: 'phones',
 			status: 'in transit',
 			location: 'rwanda',
@@ -58,7 +58,7 @@ const parcels = [
 			price: 200
 		  },
 		  {
-			parcelId: '26',
+			parcelId: 26,
 			parcelName: 'phones',
 			status: 'cancelled',
 			location: 'rwanda',
@@ -69,11 +69,11 @@ const parcels = [
 		]
 	},
 	{
-		id: "3",
+		id: 3,
 		userName: "moise",
 		parcels: [
 		  {
-			parcelId: '31',
+			parcelId: 31,
 			parcelName: 'phones',
 			status: 'in transit',
 			location: 'rwanda',
@@ -82,7 +82,7 @@ const parcels = [
 			price: 200
 		  },
 		  {
-			parcelId: '33',
+			parcelId: 33,
 			parcelName: 'phones',
 			status: 'in transit',
 			location: 'rwanda',
@@ -91,7 +91,7 @@ const parcels = [
 			price: 200
 		  },
 		  {
-			parcelId: '32',
+			parcelId: 32,
 			parcelName: 'phones',
 			status: 'in transit',
 			location: 'rwanda',
@@ -106,55 +106,52 @@ app.get('/', (req, res) => {
 	res.send('SendIT');
 });
 
+// Get all parcel delivery orders
 app.get('/api/v1/users/parcels', (req, res) => {
 	res.send(parcels);
 });
+
+// Create a parcel delivery order
 app.post('/api/v1/users/parcels', (req, res) => {
-	const { error }= validateParcel(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
 	const parcel = {
 		id: parcels.length + 1,
 		name: req.body.name,
 		parcel: [
 			{
-				parcelId: '',
-		        parcelName: '',
-		        status: '',
-		        location: '',
-		        destination: '',
-		        weight: '',
-		        price: ''
+				parcelId: parcelId.length + 1,
+		    parcelName: '',
+		    status: '',
+		    location: '',
+		    destination: '',
+		    weight: '',
+		    price: ''
 			}
 		],
 	};
 	parcels.push(parcel);
 	res.send(parcel);
 });
-app.put('/api/v1/users/parcels/:id', (req, res) => {
-	const parcel = parcels.find(c => c.id === parseInt(req.params.id));
+
+// Fetch a specific parcel delivery order
+app.get('/api/v1/users/parcels/:parcelId', (req, res) => {
+	const parcel = parcels.find(c => c.parcels(c => c.parceId === parseInt(req.params.parcelId)));
 	if(!parcel) return res.status(404).send('The parcel with the given ID was not found.');
-	const { error }= validateParcel(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
 	parcel.name = req.body.name;
 	res.send(parcel);
 });
+
+// Fetch all parcel delivery order by a specific user
 app.get('/api/v1/users/:id/parcels', (req, res) => {
 const result = parcels.find(c => c.id === parseInt(req.params.id));
 res.send (result);
 });
+// Cancel a specific parcel delivery order
 app.put('/api/v1/users/parcels/:id/cancel', (req, res) => {
 	const parcel = parcels.find(c => c.id === parseInt(req.params.id));
 	if(!parcel) return res.status(404).send('The parcel with the given ID was not found.');
-	const { error }= validateParcel(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
 	parcel.name = req.body.name;
 	const index = parcels.indexOf(parcel);
 	parcels.splice(index, 1);
-	res.send(parcel);
-});
-app.get('/api/v1/users/parcels/:id', (req, res) => {
-	const parcel = parcels.find(c => c.id === parseInt(req.params.id));
-	if(!parcel) return res.status(404).send('The parcel with the given ID was not found.');
 	res.send(parcel);
 });
 app.listen(3000 , () => console.log('Listening on port 3000...'));
